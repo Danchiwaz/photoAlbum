0552f7e0151baca03534022f8e0568be6f9fab73
Daniel Maina <daniel.maina@thejitu.com>
2023-01-18 22:53:19 +0300

    first commit

---
+++ src/app/Components/HeroSection/Hero.Component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+    selector:'app-herosection',
+    templateUrl: './hero.component.html',
+    styleUrls:['./Hero.component.scss']
+
+})
+export class HeroSection implements OnInit{
+    constructor(){}
+    ngOnInit(){
+
+    }
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './Components/Header/Header.component';
+import { HeroSection } from './Components/HeroSection/Hero.Component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HeroSection,
   ],
   imports: [
     BrowserModule
